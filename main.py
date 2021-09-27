271b021718a53586a8cb596ae9664da58c28cf90
Nicolas Roy <nicolas.roy@g.austincc.edu>
2021-09-27 09:25:10 -0500

    fixed logic error

--- main.py
+++ main.py
@@ -63,16 +63,15 @@ def hangman():
         print("Invalid prompt, try a letter!\n")
         hangman()
     # if the letter guessed is in the word
-    elif guess in word:
-        # add letter to list of guessed letters
-        already_guessed.extend([guess])
 
+    elif guess in word:
+        already_guessed.extend([guess])
         index = word.find(guess)
         word = word[:index] + "_" + word[index + 1:]
         display = display[:index] + guess + display[index + 1:]
         print(display + "\n")
     elif guess in already_guessed:
-        print("Try another letter!")
+        print("Try another letter.\n")
     else:
         count += 1
         if count == 1:
@@ -85,7 +84,7 @@ def hangman():
                   "  |      \n"
                   "  |      \n"
                   "__|__\n")
-            print("Wrong guess! " + str(limit - count) + " guesses remaining")
+            print("Wrong guess. " + str(limit - count) + " guesses remaining\n")
         elif count == 2:
             time.sleep(1)
             print("   _____ \n"
@@ -130,14 +129,12 @@ def hangman():
                   "  |    / \ \n"
                   "__|__\n")
             print("Wrong guess. You are hanged!!!\n")
-            print("The word was: ", already_guessed, word)
+            print("The word was:", already_guessed, word)
             play_loop()
-        if word == "_" * length:
-            print("Congrats! You have guessed the word correctly!")
-            play_loop()
-        elif count != limit:
-            hangman()
-
-        main()
+    if word == '_' * length:
+        print("Congrats! You have guessed the word correctly!")
+        play_loop()
+    elif count != limit:
         hangman()
-
+main()
+hangman()
